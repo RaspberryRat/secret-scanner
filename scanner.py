b1f828350e40e5d14711449c5041d17e0736b2e8
squid <raspberryratman@gmail.com>
2025-10-30 11:16:29 -0600

    added json rules and loaded them into attribute, added ability to add custome rules

--- scanner.py
+++ scanner.py
@@ -1,20 +1,69 @@
-import os
+import os, json, re
 
 class Scanner:
     """
     scanner class that scans directories for secrets and passwords
 
     Public Methods:
-        change_directory
+        change_directory:
+            changes working directory to be scanned
+        scan:
+            scans directory for secrets and passwords
     """
 
-    def __init__(self):
+    def __init__(self, customRules="rules/rules.json"):
         """
         constructor method for Scanner class
 
         sets _directoryToScan as the current working directory by default
         """
         self._directoryToScan = os.getcwd()
+        self._rulesData = {}
+
+        # load rules
+        rules_data = json.load(open(customRules))
+        for rule in rules_data['rules']:
+            regex = re.compile(rule['name'])
+            self._rulesData[rule['regex']] = regex
+
+    def get_current_rules(self) -> dict:
+        """
+        returns a dict of the current rule set and regex pattern
+        """
+        return self._rulesData
+
+    def change_custome_rules(self, rulesPath: str) -> None:
+        """
+        change the custom rule set used for scanning
+
+        Precondition:
+            rulesPath must be of the correct type
+            rulesPath must be a json file
+            rulesPath must be a valid path to a directory
+
+        Postcondition:
+            updates self._rulesData to use the rules in rulesPath
+
+        Raises:
+            TypeError if rulesPath not of the correct type
+            Invalid Path if rulesPath not a correct path
+            FileError?? if rulesPath not a json file
+        """
+        if not isinstance(rulesPath, str):
+            raise TypeError("rulesPath must be a str but is a "
+                            f"{type(rulesPath)}")
+
+        if not self._check_path(rulesPath): # check this is correct later
+            raise Exception("rulesPath must be a valid path to a file")
+
+        # TODO add a check to json
+
+        # update ._rulesData
+        self._rulesData = {}
+        rules_data = json.load(open(rulesPath))
+        for rule in rules_data['rules']:
+            regex = re.compile(rule['name'])
+            self._rulesData[rule['regex']] = regex
 
 
     def get_directory_to_scan(self) -> str:
@@ -68,13 +117,84 @@ class Scanner:
         except TypeError:
             return False
 
-# print_directory()
-# try:
-#     if directoryToScan:
-#         print(os.listdir(directoryToScan))
-#     else:
-#         print(os.listdir())
-# except FileNotFoundError:
+    def _get_file_list(self) -> list:
+        """
+        gets a list of all files in the directoy to be scanned
+        #TODO currently ignores directories, does not scan recusively
+        """
+        directoryContents =  os.listdir(self._directoryToScan)
+        fileList = []
+        for item in directoryContents:
+            if os.path.isfile(f"{self._directoryToScan}/{item}"):
+                fileList.append(item)
+        return fileList
+
+    def _get_file_contents(self, fileName) -> list:
+        """
+        gets all the content of a file and returns it as a list
+        """
+        if not isinstance(fileName, str):
+            raise TypeError(f"fileName is {type(fileName)}, but must be a str")
+
+        with open(fileName) as f:
+            return f.readlines()
+
+    def scan(self):
+        """
+        Scans self.__directoryToScan for secrets and passwords
+
+        Returns:
+            ?
+
+        Precondition:
+            self._directoryToScan must be a valid path to the directory
+
+
+        Raises:
+            ValueError:
+                if self._directoryToScan is not a valid path to a directory
+        """
+        if not os.path.exists(self._directoryToScan):
+            raise ValueError(f"self._directoryToScan: {self._directoryToScan} "
+                             " is not a valid path to a directory")
+
+        fileList = self._get_file_list()
+
+        for fileName in fileList:
+            fileContents = self._get_file_contents(fileName)
+            # need method to scan file contents
+
+
+    def _scan_for_secrets(self, fileContents: str):
+        """
+        internal method that scans a string for possible passwords and secrets
+
+        Parameters:
+            fileContents:
+                a str from a file to scan
+
+        Returns:
+            unsure yet
+
+        Precondition:
+            fileContents must be of the correct type
+
+        Raises:
+            TypeError:
+                if fileContents not of the correct type
+        """
+
+        if not isinstance(fileContents, str):
+            raise TypeError("fileContents must be a str but is: "
+                            f"{type(fileContents)}")
+
+        # rules_data = json.load(open("rules.json"))
+
+        # for rule in rules_data['rules']:
+        #     regex = re.compile(rule['name'])
+        #     print(regex)
+
 
 s = Scanner()
+s._scan_for_secrets('test')
 # s.change_directory()
